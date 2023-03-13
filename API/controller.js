463a4bfb9160d4af4b3951113319f14744ebba91
FameTii <shinfameti@gmail.com>
2023-03-13 22:01:12 +0000

    review body added

--- API/controller.js
+++ API/controller.js
@@ -1,4 +1,13 @@
-const { fetchCategories, fetchReviews, fetchReviewsWithId, updatingReviewVotes, fetchCommentsOfReviewId, fetchUsers, postingComment, deletingUsers} = require("./models");
+const {
+  fetchCategories,
+  fetchReviews,
+  fetchReviewsWithId,
+  updatingReviewVotes,
+  fetchCommentsOfReviewId,
+  fetchUsers,
+  postingComment,
+  deletingUsers,
+} = require("./models");
 const endpoints = require("./endpoints.json");
 
 exports.getCategories = (req, res, next) => {
@@ -12,84 +21,83 @@ exports.getCategories = (req, res, next) => {
 };
 
 exports.getReviews = (req, res, next) => {
-    const {category, sortBy, orderBy} = req.query
-    fetchReviews(category, sortBy, orderBy)
-        .then((reviews) => {
-            res.status(200).send({reviews})
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+  const { category, sortBy, orderBy } = req.query;
+  fetchReviews(category, sortBy, orderBy)
+    .then((reviews) => {
+      res.status(200).send({ reviews });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.getReviewsWithId = (req, res, next) => {
-    const {review_id} = req.params;
-    fetchReviewsWithId(review_id)
-        .then((review) => {
-            res.status(200).send({review})
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+  const { review_id } = req.params;
+  fetchReviewsWithId(review_id)
+    .then((review) => {
+      res.status(200).send({ review });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.getCommentsOfReviewId = (req, res, next) => {
-    const {review_id} = req.params;
-    fetchCommentsOfReviewId(review_id)
-        .then((comments) => {
-            res.status(200).send({comments})
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+  const { review_id } = req.params;
+  fetchCommentsOfReviewId(review_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.postComment = (req, res, next) => {
-    const {review_id} = req.params;
-    const newComment = req.body
-    postingComment(review_id, newComment)
-        .then((comment) => {
-            res.status(201).send(comment)
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+  const { review_id } = req.params;
+  const newComment = req.body;
+  postingComment(review_id, newComment)
+    .then((comment) => {
+      res.status(201).send(comment);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.updateReviewVotes = (req, res, next) => {
-    const {review_id} = req.params
-    const newVotes = req.body
-    updatingReviewVotes(review_id, newVotes)
-        .then((review) => {
-            res.status(200).send(review)
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+  const { review_id } = req.params;
+  const newVotes = req.body;
+  updatingReviewVotes(review_id, newVotes)
+    .then((review) => {
+      res.status(200).send(review);
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.getUsers = (req, res, next) => {
-    fetchUsers()
+  fetchUsers()
     .then((users) => {
-        res.status(200).send({users})
+      res.status(200).send({ users });
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
 exports.deleteComment = (req, res, next) => {
-    const {comment_id} = req.params
-    deletingUsers(comment_id)
+  const { comment_id } = req.params;
+  deletingUsers(comment_id)
     .then((comment) => {
-        res.status(204).send()
+      res.status(204).send();
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
 exports.getApi = (req, res, next) => {
-    res.send(endpoints);
-}
-
+  res.send(endpoints);
+};
--- API/models.js
+++ API/models.js
@@ -1,171 +1,170 @@
-const db = require('../db/connection');
-const comments = require('../db/data/test-data/comments');
+const db = require("../db/connection");
+const comments = require("../db/data/test-data/comments");
 
 exports.fetchCategories = () => {
-    const queryStr = `SELECT slug, description FROM categories`
-    return db.query(queryStr).then((result) => {
-        if (result.rows === undefined) {
-            return Promise.reject({
-                status: 404,
-                msg: 'route does not exist'
-            })
-        }
-        return result.rows;
+  const queryStr = `SELECT slug, description FROM categories`;
+  return db.query(queryStr).then((result) => {
+    if (result.rows === undefined) {
+      return Promise.reject({
+        status: 404,
+        msg: "route does not exist",
       });
-}
+    }
+    return result.rows;
+  });
+};
 
-exports.fetchReviews = (category, sortBy = 'created_at', orderBy= 'DESC') => {
-    const validSortByOptions = ['review_id', 'created_at', 'votes']
-    const validOrderByOptions = ['ASC', 'DESC']
-    const where = `WHERE category = '${category}' `
-    const order = `GROUP BY reviews.review_id ORDER BY reviews.${sortBy} ${orderBy}`;
+exports.fetchReviews = (category, sortBy = "created_at", orderBy = "DESC") => {
+  const validSortByOptions = ["review_id", "created_at", "votes"];
+  const validOrderByOptions = ["ASC", "DESC"];
+  const where = `WHERE category = '${category}' `;
+  const order = `GROUP BY reviews.review_id ORDER BY reviews.${sortBy} ${orderBy}`;
 
-    let queryStr = `SELECT reviews.review_id, reviews.title, reviews.category, reviews.designer, reviews.owner, reviews.review_img_url, reviews.created_at, reviews.votes,
+  let queryStr = `SELECT reviews.review_id, reviews.title, reviews.category, reviews.designer, reviews.owner, reviews.review_img_url, reviews.created_at, reviews.review_body, reviews.votes,
     COUNT (comments.review_id)::int AS comment_count
     FROM reviews
-    LEFT JOIN comments ON reviews.review_id = comments.review_id `
+    LEFT JOIN comments ON reviews.review_id = comments.review_id `;
 
-    if (category !== undefined ){
-        queryStr += where
-    };
+  if (category !== undefined) {
+    queryStr += where;
+  }
 
-    if (sortBy && !validSortByOptions.includes(sortBy)) {
-        return Promise.reject({ 
-            status: 400, 
-            msg: "bad request"});
+  if (sortBy && !validSortByOptions.includes(sortBy)) {
+    return Promise.reject({
+      status: 400,
+      msg: "bad request",
+    });
+  }
+
+  if (orderBy && !validOrderByOptions.includes(orderBy)) {
+    return Promise.reject({
+      status: 400,
+      msg: "bad request",
+    });
+  }
+  queryStr += order;
+
+  return db.query(queryStr).then((result) => {
+    if (result.rowCount === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "no category found",
+      });
+    } else {
+      return result.rows;
     }
-
-    if (orderBy && !validOrderByOptions.includes(orderBy)) {
-        return Promise.reject({
-            status: 400, 
-            msg: "bad request"
-        })
-    }
-    queryStr += order;
-
-    
-    return db.query(queryStr).then((result) => {
-        if (result.rowCount === 0){
-            return Promise.reject({
-                status: 404,
-                msg: "no category found"
-            })
-        }else{
-            return result.rows;
-        }
-    })
-}
+  });
+};
 
 exports.fetchReviewsWithId = (review_id) => {
-    
-    const comments = `, COUNT (comments.review_id)::int AS comment_count`
-    
-    const queryStr = `SELECT reviews.review_id, reviews.title, reviews.review_body, reviews.category, reviews.designer, reviews.owner, reviews.review_img_url, reviews.created_at, reviews.votes 
+  const comments = `, COUNT (comments.review_id)::int AS comment_count`;
+
+  const queryStr = `SELECT reviews.review_id, reviews.title, reviews.review_body, reviews.category, reviews.designer, reviews.owner, reviews.review_img_url, reviews.created_at, reviews.votes 
     ${comments}
     FROM reviews
     LEFT JOIN comments ON reviews.review_id = comments.review_id
     WHERE reviews.review_id = ${review_id}
-    GROUP BY reviews.review_id`
-    return db.query(queryStr).then((result) => {
-        const review = result.rows[0]
-        if (review === undefined) {
-            return Promise.reject({
-                status: 404,
-                msg: `no review found`
-            })
-        } else {
-            return review
-        }
-    })
-}
+    GROUP BY reviews.review_id`;
+  return db.query(queryStr).then((result) => {
+    const review = result.rows[0];
+    if (review === undefined) {
+      return Promise.reject({
+        status: 404,
+        msg: `no review found`,
+      });
+    } else {
+      return review;
+    }
+  });
+};
 
 exports.postingComment = (review_id, newComment) => {
-    const username = newComment.username
-    const body = newComment.body
-    if (body === '' || body === undefined){
-        return Promise.reject({
-            status: 400,
-            msg: `body is empty`
-        })
-    }
-    if (username === '' || username === undefined) {
-        return Promise.reject({
-            status: 400,
-            msg: `no username`
-        })
-    }
+  const username = newComment.username;
+  const body = newComment.body;
+  if (body === "" || body === undefined) {
+    return Promise.reject({
+      status: 400,
+      msg: `body is empty`,
+    });
+  }
+  if (username === "" || username === undefined) {
+    return Promise.reject({
+      status: 400,
+      msg: `no username`,
+    });
+  }
 
-    const queryStr = `INSERT INTO comments (body, review_id, author) VALUES ('${body}', ${review_id}, '${username}') RETURNING *;`
-    return db.query(queryStr).then((result) => {
-        const comment = result.rows[0]
-        if (comment === undefined) {
-            return Promise.reject({
-                status: 400,
-                msg: `username not found`
-            })
-        } else {
-            return comment
-        }
-    })
-}
+  const queryStr = `INSERT INTO comments (body, review_id, author) VALUES ('${body}', ${review_id}, '${username}') RETURNING *;`;
+  return db.query(queryStr).then((result) => {
+    const comment = result.rows[0];
+    if (comment === undefined) {
+      return Promise.reject({
+        status: 400,
+        msg: `username not found`,
+      });
+    } else {
+      return comment;
+    }
+  });
+};
 
 exports.fetchCommentsOfReviewId = (review_id) => {
-    const queryStr = `SELECT comment_id, body, comments.review_id, author, comments.votes, comments.created_at FROM comments RIGHT JOIN reviews ON reviews.review_id = comments.review_id WHERE reviews.review_id = ${review_id} 
-    ORDER BY created_at DESC`
-    return db.query(queryStr).then((result) => {
-        const comments = result.rows
-        if(comments.length === 0) {
-            return Promise.reject({
-                status: 404,
-                msg: `no comments found`
-            })
-        } else if(comments[0].body === null){
-            return []
-        } else {
-            return comments
-        }
-    })
-}
+  const queryStr = `SELECT comment_id, body, comments.review_id, author, comments.votes, comments.created_at FROM comments RIGHT JOIN reviews ON reviews.review_id = comments.review_id WHERE reviews.review_id = ${review_id} 
+    ORDER BY created_at DESC`;
+  return db.query(queryStr).then((result) => {
+    const comments = result.rows;
+    if (comments.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: `no comments found`,
+      });
+    } else if (comments[0].body === null) {
+      return [];
+    } else {
+      return comments;
+    }
+  });
+};
 
 exports.updatingReviewVotes = (review_id, newVotes) => {
-    const voteValue = newVotes.inc_votes
-    const queryStr = `UPDATE reviews SET votes = votes + ${voteValue} WHERE review_id = ${review_id} RETURNING *`
-    return db.query(queryStr).then((result) => {
-        const review = result.rows[0]
-        if (review === undefined) {
-            return Promise.reject({
-                status: 404,
-                msg: `no review found`
-            })
-        } else {
-            return review
-        }
-    })
-}
+  const voteValue = newVotes.inc_votes;
+  const queryStr = `UPDATE reviews SET votes = votes + ${voteValue} WHERE review_id = ${review_id} RETURNING *`;
+  return db.query(queryStr).then((result) => {
+    const review = result.rows[0];
+    if (review === undefined) {
+      return Promise.reject({
+        status: 404,
+        msg: `no review found`,
+      });
+    } else {
+      return review;
+    }
+  });
+};
 
 exports.fetchUsers = () => {
-    const queryStr = `SELECT * FROM users`
-    return db.query(queryStr).then((result) => {
-        const users = result.rows
-        if (users === undefined) {
-            return Promise.reject({
-                status: 404,
-                msg: 'route does not exist'
-            })
-        }
-        return users
-    })
-}
+  const queryStr = `SELECT * FROM users`;
+  return db.query(queryStr).then((result) => {
+    const users = result.rows;
+    if (users === undefined) {
+      return Promise.reject({
+        status: 404,
+        msg: "route does not exist",
+      });
+    }
+    return users;
+  });
+};
 
 exports.deletingUsers = (comment_id) => {
-    const queryStr = `DELETE FROM comments where comment_id=${comment_id} RETURNING *`
-    return db.query(queryStr).then((result) => {
-        const comment = result.rows
-        if(comment.length === 0){
-            return Promise.reject({
-                status: 404,
-                msg: 'comment_id does not exist'
-            })
-        } 
-    })
-}
+  const queryStr = `DELETE FROM comments where comment_id=${comment_id} RETURNING *`;
+  return db.query(queryStr).then((result) => {
+    const comment = result.rows;
+    if (comment.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "comment_id does not exist",
+      });
+    }
+  });
+};
